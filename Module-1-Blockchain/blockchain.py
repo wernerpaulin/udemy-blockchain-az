6f8a5b7aec103803094913866189844cff4a45f1
Werner Paulin <werner.paulin@gmail.com>
2023-01-03 22:33:45 +0100

    2233

--- Module-1-Blockchain/blockchain.py
+++ Module-1-Blockchain/blockchain.py
@@ -13,10 +13,9 @@ print("Starting the Blockchain...")
 
 # Part 1 - Building a Blockchain
 class blockchain:
-    def __init__(self) -> None:
+    def __init__(self):
         self.chain = []
         self.create_block(proof = 1, previous_hash = '0')
-
-    def dest()
+        
 
 # Part 2 - Mining our Blockchain
